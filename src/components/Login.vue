544e25e18debde27776b4876a56734f04ba5b86a
dong <ezreal.dong@stey.com>
2020-03-25 23:18:49 +0800

    show login page

---
+++ src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="login_container">
+    <el-button>登录组件</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style lang="less" scoped>
+
+  .login_container {
+    background: #2b4b6b;
+    height: 100%;
+  }
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
+// 懒加载路由
+const Login = () => import('../components/Login.vue')
+
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '',
+    redirect: '/login'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    component: Login
   }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history'
 })
 
 export default router
