d78ab5df693f69b836d6886e7b0a92bfce8578d3
jp <jiangpeng@cecelive.com>
2020-03-31 23:35:18 +0800

    登录的校验逻辑 -p138

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,71 @@
 <template>
   <div class="login_container">
-    <el-button>登录组件</el-button>
+    <div class="login_box">
+      <div class="avatar_box">
+        <img class="avatar_image" src="../assets/img/1024.png" alt="">
+      </div>
+      <!-- 登陆 -->
+      <div class="from_container">
+        <el-form :model="form" :rules="rules" ref="formRef">
+          <!-- 用户名 -->
+          <el-form-item prop="account">
+            <el-input v-model="form.account" clearable placeholder="请输入账号" prefix-icon="iconfont iconzhanghao"></el-input>
+          </el-form-item>
+          <!-- 密码 -->
+          <el-form-item prop="password">
+            <el-input v-model="form.password" show-password placeholder="请输入密码" prefix-icon="iconfont iconmima"></el-input>
+          </el-form-item>
+          <!-- 按钮 -->
+          <el-form-item class="button_container">
+            <el-button type="primary" @click="loginClick">登录</el-button>
+            <el-button type="info" @click="resetClick">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data () {
+    return {
+      form: {
+        account: '',
+        password: ''
+      },
+      rules: {
+        account: [
+          { required: true, message: '请输入账号', trigger: 'blur' },
+          { min: 3, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 18, message: '长度在 6 到 18 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+
+  methods: {
+    loginClick () {
+      console.log(`login 账号:${this.form.account} 密码:${this.form.password}`)
+      this.$refs.formRef.validate((valid) => {
+        if (valid) {
+          alert('login!')
+        } else {
+          console.log('error login!!')
+          return false
+        }
+      })
+    },
+
+    resetClick () {
+      console.log('reset', this)
+      this.$refs.formRef.resetFields()
+    }
+  }
 }
 </script>
 
@@ -15,6 +74,48 @@ export default {
   .login_container {
     background: #2b4b6b;
     height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .login_box {
+    width: 450px;
+    height: 300px;
+    background-color: #ffffff;
+    border-radius: 3px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+
+    .avatar_box {
+      display: flex;
+      width: 130px;
+      height: 130px;
+      margin-top: -65px;
+
+      .avatar_image {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        border: 1px solid #eee;
+      }
+    }
+
+    .from_container {
+      margin-top: 20px;
+      width: 80%;
+
+      .button_container {
+        display: flex;
+        justify-content: center;
+
+        .el-button--info {
+          margin-left: 50px;
+        }
+      }
+    }
   }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/global.css'
+// 引入fonticon
+import './assets/fonts/iconfont.css'
 
 Vue.config.productionTip = false
 
