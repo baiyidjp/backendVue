c4165b228083e5a606df7e26d26a57c2b72b65ea
jp <jiangpeng@cecelive.com>
2020-04-22 23:39:15 +0800

    操作列和分页 166

--- src/components/users/Users.vue
+++ src/components/users/Users.vue
@@ -28,14 +28,31 @@
         <el-table-column label="状态">
           <template slot-scope="scope">
             <el-switch
-              v-model="scope.row.msg_state"
+              v-model="scope.row.mg_state"
               active-color="#13ce66"
-              inactive-color="#ff4949">
+              inactive-color="#ADADAD">
             </el-switch>
           </template>
         </el-table-column>
-        <el-table-column label="操作"></el-table-column>
+        <el-table-column label="操作" width="180px">
+          <template>
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            <el-tooltip effect="dark" content="用户设置" placement="top" :enterable="false">
+              <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
       </el-table>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="param.pagenum"
+        :page-sizes="[1, 2, 5, 10]"
+        :page-size="param.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+      </el-pagination>
     </el-card>
   </div>
 </template>
@@ -49,7 +66,7 @@ export default {
       param: {
         query: '',
         pagenum: 1,
-        pagesize: 2
+        pagesize: 5
       },
       userList: [],
       total: []
@@ -64,7 +81,18 @@ export default {
         console.log(res)
         if (res.meta.status !== 200) this.$message.error(res.meta.msg)
         this.userList = res.data.users
+        this.total = res.data.total
       })
+    },
+    // 监听pageSize改变的事件(改变当前一页显示多少条)
+    handleSizeChange (pageSize) {
+      this.param.pagesize = pageSize
+      this.loadUserList()
+    },
+    // 监听页码值的改变
+    handleCurrentChange (pageNum) {
+      this.param.pagenum = pageNum
+      this.loadUserList()
     }
   }
 }
